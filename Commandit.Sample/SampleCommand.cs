06f6bab363613764de510bd8510d97f9525d14b5
Rob Mackenzie <rob.d.mackenzie@gmail.com>
2014-08-11 11:33:58 -0400

    Added changed file 'SampleComamnd'

---
+++ Commandit.Sample/SampleCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandit.Sample
+{
+    [CommandAttribute(  Description="This is a sample command.",
+                        Group="Environment 1",
+                        Order=1)]
+    class SampleCommand : ICommand
+    {
+        public string Name
+        {
+            get { return "SampleCommand"; }
+        }
+
+        public void Run(ICommandParameters parameters)
+        {
+            Console.Write("Hi");
+        }
+    }
+}
